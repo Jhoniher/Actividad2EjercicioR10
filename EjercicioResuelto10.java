eb0095f172d1d315990b8813606a1a164e5517d6
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 10:39:57 -0500

    Agregados ingreso de datos

--- EjercicioResuelto10.java
+++ EjercicioResuelto10.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class EjercicioResuelto10 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingrese el numero de inscripcion: ");
+        int nInscripcion=entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Ingrese el nombre: ");
+        String nombre=entrada.nextLine();
+        System.out.println("Ingrese su patrimonio sin puntos ni comas: ");
+        double patrimonio=entrada.nextDouble();
+        System.out.println("Ingrese su estrato: ");
+        int estrato=entrada.nextInt();
+
+    }
 }
