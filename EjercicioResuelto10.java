054468e659dc6dd99e07a22446de07ec28921056
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 10:45:02 -0500

    Primera revisión hecha

--- EjercicioResuelto10.java
+++ EjercicioResuelto10.java
@@ -11,6 +11,13 @@ public class EjercicioResuelto10 {
         double patrimonio=entrada.nextDouble();
         System.out.println("Ingrese su estrato: ");
         int estrato=entrada.nextInt();
+        double matricula=50000;
+        if(patrimonio>2000000 && estrato>3){
+            matricula+=patrimonio*0.03;
+        }
+        System.out.println("Su numero de inscripción es: "+nInscripcion);
+        System.out.println("Su nombre es: "+nombre);
+        System.out.println("El valor de su matricula es: "+matricula);
 
     }
 }
