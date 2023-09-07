ae392a17be0ef90fcd8a2d2d99fecf921e5e25d0
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 10:49:10 -0500

    Ejercicio finalizado

--- EjercicioResuelto10.java
+++ EjercicioResuelto10.java
@@ -4,15 +4,15 @@ public class EjercicioResuelto10 {
         Scanner entrada=new Scanner(System.in);
         System.out.println("Ingrese el numero de inscripcion: ");
         int nInscripcion=entrada.nextInt();
-        entrada.nextLine();
+        entrada.nextLine(); //Linea Buffer
         System.out.println("Ingrese el nombre: ");
-        String nombre=entrada.nextLine();
+        String nombre=entrada.nextLine(); //Ingreso de nombre
         System.out.println("Ingrese su patrimonio sin puntos ni comas: ");
-        double patrimonio=entrada.nextDouble();
+        double patrimonio=entrada.nextDouble(); //Ingreso de patrimonio
         System.out.println("Ingrese su estrato: ");
-        int estrato=entrada.nextInt();
-        double matricula=50000;
-        if(patrimonio>2000000 && estrato>3){
+        int estrato=entrada.nextInt(); //Ingreso de estrato
+        double matricula=50000; //Valor de la matricula
+        if(patrimonio>2000000 && estrato>3){ //Se establece condicion, se cumple si ambas cosas se cumplen
             matricula+=patrimonio*0.03;
         }
         System.out.println("Su numero de inscripción es: "+nInscripcion);
